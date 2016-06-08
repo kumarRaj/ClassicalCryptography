f680835c4d66156587618077a165e12fcf21350f
Raj Kumar <rajkum@thoughtworks.com>
2016-06-08 12:18:30 +0530

    Created the encipher

--- atbash.js
+++ atbash.js
@@ -1,11 +1,22 @@
 var sout = console.log;
 var start = "a".charCodeAt(0);
 var end = "z".charCodeAt(0);
-var substitutues = {};
-for (i = start,j = end;i <=  end,j >= start;i++,j--){
-	var key = String.fromCharCode(i);
-	var value = String.fromCharCode(j);
-	substitutues[key] = value;
+
+var createAtBashSubstitutes = function () {
+	var substitutues = {};
+	for (i = start,j = end;i <=  end,j >= start;i++,j--){
+		var key = String.fromCharCode(i);
+		var value = String.fromCharCode(j);
+		substitutues[key] = value;
+	}
+	substitutues[" "] = " ";
+	return substitutues;
 }
 
-sout(substitutues);
+var encipher = function (plainText, substitutues) {
+	return (plainText.split("").map(function (char) {
+		return substitutues[char];
+	})).join("");
+}
+
+sout(encipher("attack at dawn", createAtBashSubstitutes()));
