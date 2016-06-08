9bf85f307eb9edf4bc06eee013414590c295cbe6
Raj Kumar <rajkum@thoughtworks.com>
2016-06-08 12:08:05 +0530

    Put the keys and values in a dictionary

--- atbash.js
+++ atbash.js
@@ -1,6 +1,11 @@
 var sout = console.log;
 var start = "a".charCodeAt(0);
 var end = "z".charCodeAt(0);
+var substitutues = {};
 for (i = start,j = end;i <=  end,j >= start;i++,j--){
-	sout(String.fromCharCode(i) + " -> " + String.fromCharCode(j));
+	var key = String.fromCharCode(i);
+	var value = String.fromCharCode(j);
+	substitutues[key] = value;
 }
+
+sout(substitutues);
