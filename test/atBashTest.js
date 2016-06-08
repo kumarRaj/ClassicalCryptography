28cb5b9e7608c53708b3c642a37dd20de3649b8a
Raj Kumar <rajkum@thoughtworks.com>
2016-06-08 18:07:08 +0530

    Add decipher test

--- test/atBashTest.js
+++ test/atBashTest.js
@@ -6,4 +6,16 @@ describe('atBash',function() {
     var expected = "zggzxp zg wzdm";
     assert.equal(expected, atBash.encipher("attack at dawn"));
 	});
+	it('encipher',function(){
+    var expected = "rg rh z yvzfgrufo dliow";
+    assert.equal(expected, atBash.encipher("it is a beautiful world"));
+	});
+	it('decipher',function(){
+    var expected = "it is a beautiful world";
+    assert.equal(expected, atBash.encipher("rg rh z yvzfgrufo dliow"));
+	});
+	it('decipher',function(){
+    var expected = "attack at dawn";
+    assert.equal(expected, atBash.encipher("zggzxp zg wzdm"));
+	});
 });
